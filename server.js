79d4f7b4e02b1592b5d69e825b86ae88e81bc29a
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2016-10-07 19:57:54 +0000

    final commit

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 var express = require("express");
 var app = express();
+var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+var result = {unix: null, natural: null};
+function getDate(unix){
+   var d = new Date(unix * 1000),
+    YY = d.getFullYear(),
+    MM = monthNames[d.getMonth()],
+    DD = ('0' + d.getDate()).slice(-2);
+    var s = {};
+    s.unix = parseInt(unix, 10),
+    s.natural = MM+" "+DD+", "+YY;
+    return s;
+}
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
+app.use('/', function (req, res) {
+  var date = decodeURIComponent(req.url.slice(1));
+  if(date === ""){
+    res.sendFile( __dirname + "/index.html");
+  }else{
+    if(!isNaN(parseInt(date, 10))){
+      result = getDate(date);
+    }else if(!isNaN(Date.parse(date))){
+      result = getDate(Date.parse(date)/1000);
+    }
+    res.send(result);
+    result={};
+  }
+}).listen(8080);
